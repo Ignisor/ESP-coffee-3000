15e64ddb3dad1c94b8881dbe4cb40a8b5363a0aa
Ignisor <Ignis2497@gmail.com>
2019-05-07 10:51:29 +0300

    user config

---
+++ esp/utils/user_config.py
@@ -0,0 +1,30 @@
+import copy
+import json
+from uerrno import ENOENT
+
+
+class UserConfig:
+    def __init__(self, filename='userconf.json'):
+        self.filename = filename
+
+        self.__conf = {}
+        self._load()
+
+    def _load(self):
+        try:
+            with open(self.filename) as conf_file:
+                self.__conf = json.loads(conf_file.read())
+        except OSError as exc:
+            if exc.args[0] != ENOENT:
+                raise exc
+
+    def _save(self):
+        with open(self.filename, 'w') as conf_file:
+            conf_file.write(json.dumps(self.__conf))
+
+    def get(self):
+        return copy.copy(self.__conf)
+
+    def update(self, values):
+        self.__conf.update(values)
+        self._save()
