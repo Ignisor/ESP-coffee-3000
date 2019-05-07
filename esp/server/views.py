b29bb54c38684e62fe47731c48aa1ae683a43ad0
Ignisor <Ignis2497@gmail.com>
2019-05-07 11:08:16 +0300

    get and update API endpoints for user config

--- esp/server/views.py
+++ esp/server/views.py
@@ -1,5 +1,7 @@
 from utils.pins import open_relay
 from data import conf
+
+from esp.utils.user_config import UserConfig
 from . import server_app as srv
 from .core import JSONResponse, Response
 
@@ -11,29 +13,23 @@ def process_post(request):
 
 @srv.view('GET', '/config/')
 def get_config(request):
-    conf_dict = {}
-    for var in dir(conf):
-        if var.startswith('__'):
-            continue  # skip system variables
-
-        conf_dict[var] = getattr(conf, var)
-
-    return JSONResponse(200, conf_dict)
+    return JSONResponse(200, UserConfig().get())
 
 
 @srv.view('POST', '/config/')
 def update_config(request):
-    with open('data/local_conf.py', 'w') as local_conf:
-        for var, value in request.body.items():
-            line = '{} = {}\n'.format(var, repr(value))
-            local_conf.write(line)
+    if not isinstance(request.body, dict):
+        return Response(400, "400 Bad Request (request is not in JSON format)")
 
-    return Response(200)
+    user_conf = UserConfig()
+    user_conf.update(request.body)
+
+    return JSONResponse(200, user_conf.get())
 
 
 @srv.view('GET', '/error/')
 def get_error(request):
     with open(conf.ERROR_LOG_FILENAME) as error_log:
-        r = Response(200, error_log.read())
+        resp = Response(200, error_log.read())
 
-    return r
+    return resp
